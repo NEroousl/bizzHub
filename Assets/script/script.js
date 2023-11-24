0cd30c1d201f21c27cc2b87ca0827fcb31cc4764
Malinda Vitharana <96795111+NEroousl@users.noreply.github.com>
2023-11-25 04:15:36 +0530

    initial commit

---
+++ Assets/script/script.js
@@ -0,0 +1,28 @@
+// let searchForm = document.querySelector('.search-form');
+
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
+//     navbar.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+// let cartItem = document.querySelector('.cart-items-container');
+
+// document.querySelector('cart-btn').onclick = () =>{
+//     cartItem.classList.toggle('active');
+//     navbar.classList.remove('active');
+//     searchForm.classList.remove('active');
+// }
+
+// window.onscroll = () =>{
+//     navbar.classList.remove('active');
+//     searchForm.classList.remove('active');
+//     cartItem.classList.remove('active');
+// }
+
+// function fun() {
+//     alert (" Hello World \n Welcome to the javaTpoint.com \n This is an alert dialog box ");
+
+$(document).on("click","#addCart",function(){
+    alert("I am a pop up ! ");
+   });
